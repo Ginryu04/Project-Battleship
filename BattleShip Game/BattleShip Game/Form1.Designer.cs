efadb06e9bae6d88231277c9726739c8ea9cb378
ginryu04 <hakimnanuro0499@gmail.com>
2020-01-06 23:07:53 +0700

    next commit

---
+++ BattleShip Game/BattleShip Game/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace BattleShip_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playerScore = new System.Windows.Forms.Label();
+            this.enemyScore = new System.Windows.Forms.Label();
+            this.enemyMoves = new System.Windows.Forms.Label();
+            this.roundsText = new System.Windows.Forms.Label();
+            this.helpText = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playerScore
+            // 
+            this.playerScore.AutoSize = true;
+            this.playerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerScore.ForeColor = System.Drawing.Color.Black;
+            this.playerScore.Location = new System.Drawing.Point(337, 100);
+            this.playerScore.Name = "playerScore";
+            this.playerScore.Size = new System.Drawing.Size(49, 32);
+            this.playerScore.TabIndex = 0;
+            this.playerScore.Text = "00";
+            // 
+            // enemyScore
+            // 
+            this.enemyScore.AutoSize = true;
+            this.enemyScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enemyScore.Location = new System.Drawing.Point(911, 100);
+            this.enemyScore.Name = "enemyScore";
+            this.enemyScore.Size = new System.Drawing.Size(49, 32);
+            this.enemyScore.TabIndex = 1;
+            this.enemyScore.Text = "00";
+            // 
+            // enemyMoves
+            // 
+            this.enemyMoves.AutoSize = true;
+            this.enemyMoves.BackColor = System.Drawing.Color.Transparent;
+            this.enemyMoves.ForeColor = System.Drawing.SystemColors.Control;
+            this.enemyMoves.Location = new System.Drawing.Point(806, 54);
+            this.enemyMoves.Name = "enemyMoves";
+            this.enemyMoves.Size = new System.Drawing.Size(25, 17);
+            this.enemyMoves.TabIndex = 2;
+            this.enemyMoves.Text = "A1";
+            // 
+            // roundsText
+            // 
+            this.roundsText.AutoSize = true;
+            this.roundsText.BackColor = System.Drawing.Color.Transparent;
+            this.roundsText.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roundsText.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.roundsText.Location = new System.Drawing.Point(487, 179);
+            this.roundsText.Name = "roundsText";
+            this.roundsText.Size = new System.Drawing.Size(102, 29);
+            this.roundsText.TabIndex = 3;
+            this.roundsText.Text = "Rounds";
+            // 
+            // helpText
+            // 
+            this.helpText.AutoSize = true;
+            this.helpText.BackColor = System.Drawing.Color.Transparent;
+            this.helpText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.helpText.ForeColor = System.Drawing.Color.LightGreen;
+            this.helpText.Location = new System.Drawing.Point(109, 534);
+            this.helpText.Name = "helpText";
+            this.helpText.Size = new System.Drawing.Size(328, 18);
+            this.helpText.TabIndex = 4;
+            this.helpText.Text = "Click on 3 Different Buttons Above to Start";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "W";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 326);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 409);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 484);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Z";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(543, 251);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "A";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(543, 326);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "B";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(543, 409);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "C";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(543, 484);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 17);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "D";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::BattleShip_Game.Properties.Resources.bg1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1063, 577);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.helpText);
+            this.Controls.Add(this.roundsText);
+            this.Controls.Add(this.enemyMoves);
+            this.Controls.Add(this.enemyScore);
+            this.Controls.Add(this.playerScore);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "BattleShip";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label playerScore;
+        private System.Windows.Forms.Label enemyScore;
+        private System.Windows.Forms.Label enemyMoves;
+        private System.Windows.Forms.Label roundsText;
+        private System.Windows.Forms.Label helpText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
